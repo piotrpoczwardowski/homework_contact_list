4532a286691171dc81e8f2414c30f7b14a943350
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2018-10-13 14:57:20 +0200

    adds remove contact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
+
 class App extends Component {
+
+  state = {
+    contacts : [
+      {
+        id: 1,
+        name: 'Piotr',
+        surname: 'Poczwardowski',
+        number: '534429916'
+      },
+      {
+        id: 2,
+        name: 'Daria',
+        surname: 'Werner',
+        number: '551485921'
+      },
+      {
+        id: 3,
+        name: 'Paweł',
+        surname: 'Poczwardowski',
+        number: '551779251'
+      }
+    ]
+  }
+  removeContact = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        y => contactId !== y.id
+      )
+    })
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='contact_list'>
+          <h1>Contact List</h1>
+          <div className='list'>
+            <ul>
+              {this.state.contacts.map(x => (
+                <li key={x.id}>
+                  <p>Imię: {x.name}</p>
+                  <p>Nazwisko: {x.surname}</p>
+                  <p>Numer tel: {x.number}</p>
+                  <button onClick={() => this.removeContact(x.id)}>remove</button>
+                  </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
